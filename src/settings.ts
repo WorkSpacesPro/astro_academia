0f507914be633bea6115d46183e8816e4a95cb6f
HelloPages <201108396+HelloPages@users.noreply.github.com>
2025-05-05 21:01:26 +0800

    Update settings.ts

--- src/settings.ts
+++ src/settings.ts
@@ -26,6 +26,7 @@ export const template = {
 	transitions: true,
 	lightTheme: 'light', // Select one of the Daisy UI Themes or create your own
 	darkTheme: 'dark', // Select one of the Daisy UI Themes or create your own
+	testTheme: 'cupcake', // test
 	excerptLength: 200,
 	postPerPage: 5,
     base: '/astro_academia' // Repository name starting with /
